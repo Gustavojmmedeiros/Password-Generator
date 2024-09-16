7cb6e455e819f7128b86eacec0ef647fb0170953
Gustavo Medeiros <90402262+Gustavojmmedeiros@users.noreply.github.com>
2024-09-16 18:39:00 +0000

    Adicionando input de caracteres

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import random
 print("---Gerador de senhas---")
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHYJKLMNOPQRSTUVWXYZ!@#$%¨&*().,?0123456789"
+
+number = int(input("Tamanho da senha (mínimo de 6 caracteres): "))
+
