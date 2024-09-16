756544257ae0cda8c44f9985aae62201db029e6b
Gustavo Medeiros <90402262+Gustavojmmedeiros@users.noreply.github.com>
2024-09-16 18:53:25 +0000

    Criando função gerar_senha

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ quantity = int(input("Deseja gerar quantas senhas? (mínimo de 1 e máximo de 5)
 
 print("Gerando senhas...")
 
-for pwd in range(quantity):
-    passwords = ''
+def gerar_senha(l, q):
+    for pwd in range(q):
+        passwords = ''
 
-    for i in range(length):
-        passwords += random.choice(chars)
-    
-    print(passwords)
+        for i in range(l):
+            passwords += random.choice(chars)
+        
+        print(passwords)
+
+senha = gerar_senha(length, quantity)
