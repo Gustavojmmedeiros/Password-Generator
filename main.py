e429671192823abefca1935233fd56d0b957a4a1
Gustavo Medeiros <90402262+Gustavojmmedeiros@users.noreply.github.com>
2024-09-16 18:48:15 +0000

    Criando a funcionalidade de gerar senhas

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHYJKLMNOPQRSTUVWXYZ!@#$%¨&*().,?01234
 length = int(input("Tamanho da senha (mínimo de 6 caracteres): "))
 
 quantity = int(input("Deseja gerar quantas senhas? (mínimo de 1 e máximo de 5): "))
+
+print("Gerando senhas...")
+
+for pwd in range(quantity):
+    passwords = ''
+
+    for i in range(length):
+        passwords += random.choice(chars)
+    
+    print(passwords)
