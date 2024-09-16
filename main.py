a99903d376ea7d63f666e091f980753d13a981ea
Gustavo Medeiros <90402262+Gustavojmmedeiros@users.noreply.github.com>
2024-09-16 18:42:58 +0000

    Adicionando input de número de senhas

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ print("---Gerador de senhas---")
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHYJKLMNOPQRSTUVWXYZ!@#$%¨&*().,?0123456789"
 
-number = int(input("Tamanho da senha (mínimo de 6 caracteres): "))
+length = int(input("Tamanho da senha (mínimo de 6 caracteres): "))
 
+quantity = int(input("Deseja gerar quantas senhas? (mínimo de 1 e máximo de 5): "))
